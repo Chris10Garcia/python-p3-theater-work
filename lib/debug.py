573646b7aa7c1357eff0657f0dae6e77e7ea9ae8
Christian Garcia <bones192nd@gmail.com>
2023-07-07 11:07:40 -0400

    debug.py file complete. works

---
+++ lib/debug.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from faker import Faker
+
+from models import Audition, Role
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///threater_work.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    import ipdb; ipdb.set_trace()
